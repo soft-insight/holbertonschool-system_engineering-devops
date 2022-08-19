a3fed12a0061d55bc75e978a37c5a80edefaaaea
soft-insight <j0108rodriguez@gmail.com>
2022-08-19 16:08:46 -0500

    commit: 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Using Puppet to create a file
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}

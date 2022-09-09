2cff89d153f63a1fb61501eb1273a1f6ea13f122
soft-insight <j0108rodriguez@gmail.com>
2022-09-09 16:31:40 -0500

    commit 0:

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-""" Export data in the CSV format.
 """
+Uses https://jsonplaceholder.typicode.com along with an employee ID to
+return information about the employee's todo list progress
+"""
+
 import csv
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-
-    user_id = sys.argv[1]
-    urlApi = 'https://jsonplaceholder.typicode.com'
-    user = requests.get(urlApi + f'/users/{user_id}').json()
-
-    user_tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
-                              params={'userId': user_id}).json()
-
-    filename = f'{user_id}.csv'
-
-    with open(filename, 'w', newline='') as f:
-        data2csv = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for i in user_tasks:
-            data2csv.writerow([int(user_id),
-                               user.get('username'),
-                               i.get('completed'),
-                               i.get('title')])
+if __name__ == '__main__':
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            taskwriter.writerow([int(userId), user.get('username'),
+                                 task.get('completed'),
+                                 task.get('title')])
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-""" export data in the JSON format
+"""
+Uses https://jsonplaceholder.typicode.com along with an employee ID to
+return information about the employee's todo list progress
 """
 
+import json
 import requests
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-
-    user_id = sys.argv[1]
-    urlApi = 'https://jsonplaceholder.typicode.com'
-    user = requests.get(urlApi + f'/users/{user_id}').json()
-
-    user_tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
-                              params={'userId': user_id}).json()
-
-    print(user)
+if __name__ == '__main__':
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    username = user.get('username')
+    tasks = []
+    for task in todo:
+        task_dict = {}
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict["username"] = username
+        tasks.append(task_dict)
+    jsonobj = {}
+    jsonobj[userId] = tasks
+    with open("{}.json".format(userId), 'w') as jsonfile:
+        json.dump(jsonobj, jsonfile)
